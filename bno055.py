b4ae340442872f763db6db25c5ce2c59a123230a
Keith Baston <keith.baston@gmail.com>
2016-12-15 17:10:40 -0800

    Hiding constants not needed by end user to save space.

--- bno055.py
+++ bno055.py
@@ -5,42 +5,31 @@ from drivers.i2cutils import I2cRegDevice
 
 # pylint: disable=undefined-variable
 
-AXIS_MAP_CONFIG = const(0x41)
-AXIS_MAP_SIGN = const(0x42)
+_AXIS_MAP_CONFIG = const(0x41)
+_AXIS_MAP_SIGN = const(0x42)
 
-CALIB_STAT = const(0x35)
-CALIB_START = const(0x55)
-CALIB_LEN = const(21)
+_CALIB_STAT = const(0x35)
+_CALIB_START = const(0x55)
+_CALIB_LEN = const(21)
 
-GRAV_START = const(0x2E)
-LIA_START = const(0x28)
+_GRAV_START = const(0x2E)
+_LIA_START = const(0x28)
 
-OP_MODE = const(0x3D)
-OP_MODE_CONFIG = const(0b0000)
-OP_MODE_ACC_ONLY = const(0b0001)
-OP_MODE_MAG_ONLY = const(0b0010)
-OP_MODE_GYRO_ONLY = const(0b0011)
-OP_MODE_ACC_MAG = const(0b0100)
-OP_MODE_ACC_GYRO = const(0b0101)
-OP_MODE_MAG_GYRO = const(0b0110)
-OP_MODE_AMG = const(0b0111)
-OP_MODE_IMU = const(0b1000)
-OP_MODE_COMPASS = const(0b1001)
-OP_MODE_M4G = const(0b1010)
-OP_MODE_NDOF_FMC_OFF = const(0b1011)
-OP_MODE_NDOF = const(0b1100)
+_OP_MODE = const(0x3D)
+_OP_MODE_CONFIG = const(0b0000)
+_OP_MODE_NDOF = const(0b1100)
 
-SYS_STATUS = const(0x39)
+_SYS_STATUS = const(0x39)
 
-SYS_STATUS_IDLE = const(0)
-SYS_STATUS_ERROR = const(1)
-SYS_STATUS_FUSION_RUNNING = const(5)
+_SYS_STATUS_IDLE = const(0)
+_SYS_STATUS_ERROR = const(1)
+_SYS_STATUS_FUSION_RUNNING = const(5)
 
-SYS_TRIGGER = const(0x3F)
-SYS_TRIGGER_RST_SYS = const(1 << 5)
+_SYS_TRIGGER = const(0x3F)
+_SYS_TRIGGER_RST_SYS = const(1 << 5)
 
-EUL_ORIENT_START = const(0x1A)
-EUL_ORIENT_LEN = const(6)
+_EUL_ORIENT_START = const(0x1A)
+_EUL_ORIENT_LEN = const(6)
 
 
 class SystemError(Exception):
@@ -60,81 +49,81 @@ class Bno055:
 
     def __init__(self, i2c, axises=AXIS_MAP_P1, calib_data=None, address=0x28):
         self.dev = I2cRegDevice(i2c, address)
-        self.eul_buf = bytearray(EUL_ORIENT_LEN)
+        self.eul_buf = bytearray(_EUL_ORIENT_LEN)
         self.init(axises, calib_data)
 
     def init(self, axises=AXIS_MAP_P1, calib_data=None):
         self.reset()
 
         try:
-            status = self.dev[SYS_STATUS]
+            status = self.dev[_SYS_STATUS]
         except OSError:
             status = -1
 
-        while status != SYS_STATUS_IDLE:
-            if status == SYS_STATUS_ERROR:
+        while status != _SYS_STATUS_IDLE:
+            if status == _SYS_STATUS_ERROR:
                 raise SystemError()
 
             time.sleep_ms(5)
             try:
-                status = self.dev[SYS_STATUS]
+                status = self.dev[_SYS_STATUS]
             except OSError:
                 status = -1
 
         if calib_data:
-            if len(calib_data) != CALIB_LEN:
+            if len(calib_data) != _CALIB_LEN:
                 raise SystemError("Invalid calibrate data.")
-            self.dev.writeto_mem(CALIB_START, calib_data)
+            self.dev.writeto_mem(_CALIB_START, calib_data)
 
         self._set_axises(axises)
 
-        self.mode(OP_MODE_NDOF)
+        self.mode(_OP_MODE_NDOF)
 
-        status = self.dev[SYS_STATUS]
+        status = self.dev[_SYS_STATUS]
 
-        while status != SYS_STATUS_FUSION_RUNNING:
+        while status != _SYS_STATUS_FUSION_RUNNING:
             if status == 1:
                 raise SystemError()
 
             time.sleep_ms(5)
-            status = self.dev[SYS_STATUS]
+            status = self.dev[_SYS_STATUS]
 
     def deinit(self):
         self.reset()
 
     def reset(self):
-        self.dev[SYS_TRIGGER] = SYS_TRIGGER_RST_SYS
+        self.dev[_SYS_TRIGGER] = _SYS_TRIGGER_RST_SYS
 
     def _set_axises(self, axises):
-        self.dev[AXIS_MAP_CONFIG] = axises >> 3
-        self.dev[AXIS_MAP_SIGN] = axises & 0b111
+        self.dev[_AXIS_MAP_CONFIG] = axises >> 3
+        self.dev[_AXIS_MAP_SIGN] = axises & 0b111
 
     def mode(self, mode=None):
         if mode is None:
-            return self.dev[OP_MODE]
+            return self.dev[_OP_MODE]
 
-        self.dev[OP_MODE] = mode
+        self.dev[_OP_MODE] = mode
 
         # Just sleep the full 19 ms for switching to config mode.
         time.sleep_ms(19)
 
-    def _get_3_int16(self, addr, div):
+    def _get_3_int16(self, addr, scale):
         self.dev.readfrom_mem_into(addr, self.eul_buf)
         vals = ustruct.unpack("<hhh", self.eul_buf)
 
-        return vals[0]/div, vals[1]/div, vals[2]/div
+        return vals[0]/scale, vals[1]/scale, vals[2]/scale
 
     def get_orient(self):
-        return self._get_3_int16(EUL_ORIENT_START, 16.0)
+        return self._get_3_int16(_EUL_ORIENT_START, 16.0)
 
     def get_lin_acc(self):
-        return self._get_3_int16(LIA_START, 100)
+        return self._get_3_int16(_LIA_START, 100)
 
     def get_grav(self):
-        return self._get_3_int16(GRAV_START, 100)
+        return self._get_3_int16(_GRAV_START, 100)
 
     def calib_state(self):
-        state = self.dev[CALIB_STAT]
+        state = self.dev[_CALIB_STAT]
         sys_calib = bool(state & (3 << 6))
         gyr_calib = bool(state & (3 << 4))
         acc_calib = bool(state & (3 << 2))
@@ -143,12 +132,10 @@ class Bno055:
         return sys_calib, gyr_calib, acc_calib, mag_calib
 
     def get_calib_data(self):
-        self.mode(OP_MODE_CONFIG)
+        self.mode(_OP_MODE_CONFIG)
 
-        calib_data = self.dev.readfrom_mem(CALIB_START, CALIB_LEN)
+        calib_data = self.dev.readfrom_mem(_CALIB_START, _CALIB_LEN)
 
-        self.mode(OP_MODE_NDOF)
+        self.mode(_OP_MODE_NDOF)
 
         return calib_data
-
-
